e08bc05b29630a7547f051433cd8e8b71d2a2df0
Julien Camblan <julien@tymate.com>
2020-09-26 16:32:38 +0200

    build: setup app with expo

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import GraphQLProvider from 'GraphQLProvider';
 
 export default function App() {
   return (
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+const path = require('path');
+
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.js', '.android.js', '.ios.js', '.web.js'],
+          root: ['./src'],
+          alias: {
+            src: path.resolve(__dirname, 'src'),
+          },
+        },
+      ],
+    ],
   };
 };
