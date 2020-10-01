cecdd5d07b16b99d269da4240637d399f810c850
Julien Camblan <julien@tymate.com>
2020-10-01 14:26:57 +0200

    feat: handle Register

--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -4,32 +4,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from 'screens/Home';
 import { Button } from 'ui';
 import Login from 'screens/Auth/Login';
+import Register from 'screens/Auth/Register';
+
 const AuthStack = createStackNavigator();
-
-// const Login = () => {
-//   return (
-//     <SafeAreaView>
-//       <View>
-//         <Text style={{ color: '#000000' }}>Hello</Text>
-//         <Text style={{ color: '#000000' }}>Hello</Text>
-//         <Text style={{ color: '#000000' }}>Hello</Text>
-//         <Text style={{ color: '#000000' }}>Hello</Text>
-//         <Button title="test" />
-//         <Text style={{ color: '#000000' }}>Hello</Text>
-//         <Text style={{ color: '#000000' }}>Hello</Text>
-//         <Text style={{ color: '#000000' }}>Hello</Text>
-//       </View>
-//     </SafeAreaView>
-//   );
-// };
-
-const Register = () => {
-  return (
-    <SafeAreaView>
-      <Text>Coucou</Text>
-    </SafeAreaView>
-  );
-};
 
 const AuthNavigator = () => {
   return (
---
+++ src/screens/Auth/Register.js
@@ -0,0 +1,155 @@
+import React, { useState } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  Text,
+  ScrollView,
+  View,
+} from 'react-native';
+import { Formik } from 'formik';
+import { Button, Title } from 'ui';
+import { Input, Error } from 'ui/form';
+import book from 'images/book.png';
+import { ERRORS } from 'utils';
+import * as Yup from 'yup';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import { signIn } from 'api/auth';
+import { useAuth } from 'hooks';
+
+const REGISTER = gql`
+  mutation register($input: RegisterInput!) {
+    register(input: $input) {
+      success
+      errors {
+        attribute
+        error
+        message
+        path
+      }
+    }
+  }
+`;
+
+const Register = ({ navigation }) => {
+  const [register] = useMutation(REGISTER);
+  const { onUpdate } = useAuth();
+
+  const handleRegister = async (values, { setStatus }) => {
+    try {
+      await register({
+        variables: {
+          input: values,
+        },
+      });
+
+      const { data } = await signIn({
+        username: values.email,
+        password: values.password,
+      });
+      onUpdate(data);
+    } catch (err) {
+      console.dir(err);
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <KeyboardAvoidingView>
+        <ScrollView style={{ padding: 20 }}>
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              flex: 1,
+            }}
+          >
+            <Image
+              source={book}
+              style={{
+                width: '100%',
+                maxWidth: '60%',
+                resizeMode: 'contain',
+                height: 'auto',
+                aspectRatio: 3,
+              }}
+            />
+            <Title>BOOKLUB</Title>
+          </View>
+          <Formik
+            validationSchema={Yup.object().shape({
+              email: Yup.string().email(ERRORS.EMAIL).required(ERRORS.REQUIRED),
+              username: Yup.string(),
+              password: Yup.string().required(ERRORS.REQUIRED),
+            })}
+            initialValues={{ username: '', password: '', email: '' }}
+            onSubmit={handleRegister}
+          >
+            {({
+              handleSubmit,
+              handleChange,
+              values,
+              handleBlur,
+              status,
+              errors,
+              touched,
+              isSubmitting,
+            }) => (
+              <View style={{ width: '100%' }}>
+                <Input
+                  label="E-mail"
+                  placeholder="E-mail"
+                  value={values.email}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  autoCompleteType="email"
+                  keyboardType="email-address"
+                  returnKeyType="next"
+                  textContentType="emailAddress"
+                  style={{ marginBottom: 16 }}
+                  error={touched.email && errors.email}
+                />
+                <Input
+                  label="Username"
+                  placeholder="Username"
+                  value={values.username}
+                  onChangeText={handleChange('username')}
+                  onBlur={handleBlur('username')}
+                  autoCompleteType="username"
+                  returnKeyType="next"
+                  textContentType="emailAddress"
+                  style={{ marginBottom: 16 }}
+                  error={touched.username && errors.username}
+                />
+                <Input
+                  label="Mot de passe"
+                  placeholder="Mot de passe"
+                  value={values.password}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  autoCompleteType="password"
+                  textContentType="password"
+                  clearTextOnFocus
+                  secureTextEntry
+                  returnKeyType="send"
+                  error={touched.password && errors.password}
+                />
+                <Error>{status}</Error>
+                <View style={{ marginTop: 32 }}>
+                  <Button
+                    title="S'enregister"
+                    onPress={() => handleSubmit(values)}
+                    isLoading={isSubmitting}
+                  />
+                </View>
+              </View>
+            )}
+          </Formik>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export default Register;
