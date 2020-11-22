bd59ccde364233dbae99254f49a83cc754d0278d
Julien Camblan <julien@tymate.com>
2020-11-22 01:17:12 +0100

    feat: add bottom sheet for join/create club

--- src/navigation/ClubsNavigator.js
+++ src/navigation/ClubsNavigator.js
@@ -10,40 +10,43 @@ import Session from 'screens/Club/Session';
 import CreateSession from 'screens/Club/CreateSession';
 import CreateSubmission from 'screens/Club/CreateSubmission';
 import { theme } from 'ui';
+import { Host } from 'react-native-portalize';
 
 const BooklubStack = createStackNavigator();
 
 const ClubsNavigator = () => {
   return (
-    <BooklubStack.Navigator
-      screenOptions={{
-        headerShown: true,
-        headerTitleContainerStyle: { display: 'none' },
-      }}
-    >
-      <BooklubStack.Screen
-        name="ClubHome"
-        component={Home}
-        options={{ title: 'Home' }}
-      />
-      <BooklubStack.Screen name="CreateClub" component={CreateClub} />
-      <BooklubStack.Screen name="JoinClub" component={JoinClub} />
-      <BooklubStack.Screen
-        name="Club"
-        component={Club}
-        options={({ route }) => ({ title: route.params.title })}
-      />
-      <BooklubStack.Screen
-        name="Session"
-        component={Session}
-        options={({ route }) => ({ title: route.params.name })}
-      />
-      <BooklubStack.Screen name="CreateSession" component={CreateSession} />
-      <BooklubStack.Screen
-        name="CreateSubmission"
-        component={CreateSubmission}
-      />
-    </BooklubStack.Navigator>
+    <Host>
+      <BooklubStack.Navigator
+        screenOptions={{
+          headerShown: true,
+          headerTitleContainerStyle: { display: 'none' },
+        }}
+      >
+        <BooklubStack.Screen
+          name="ClubHome"
+          component={Home}
+          options={{ title: 'Home' }}
+        />
+        <BooklubStack.Screen name="CreateClub" component={CreateClub} />
+        <BooklubStack.Screen name="JoinClub" component={JoinClub} />
+        <BooklubStack.Screen
+          name="Club"
+          component={Club}
+          options={({ route }) => ({ title: route.params.title })}
+        />
+        <BooklubStack.Screen
+          name="Session"
+          component={Session}
+          options={({ route }) => ({ title: route.params.name })}
+        />
+        <BooklubStack.Screen name="CreateSession" component={CreateSession} />
+        <BooklubStack.Screen
+          name="CreateSubmission"
+          component={CreateSubmission}
+        />
+      </BooklubStack.Navigator>
+    </Host>
   );
 };
 
